b7ef59c8415b6e1ea173fa79df5aba77e84d83a6
Luca Ciucci <luca.ciucci99@gmail.com>
2022-12-01 23:07:52 +0100

    tool-tip enhancement

--- js/tool-tip.js
+++ js/tool-tip.js
@@ -1,71 +1,94 @@
+// typescript checks
+// @ts-check
 
-window.tooltipCfg = {
-	// times
-	showDelay: 2000,
-	hideDelay: 1000,
+var tooltipCfg = {
+	showDelay: 1000,
+	hideDelay: 500,
 }
 
 class ToolTipElement extends HTMLElement {
 	constructor() {
 		super();
 
-		this.hover = false;
+		/** @type { number | null } */
+		this.timeout = null;
 
-		// after 1s of hover, show the tooltip
 		this.addEventListener('mouseenter', () => {
-			this.hover = true;
-			setTimeout(() => {
-				//this.tt.setAttribute('visible', '');
-				this.showTooltip();
-			}, window.tooltipCfg.showDelay);
+			this.setTimeout(() => this.showTooltip(), tooltipCfg.showDelay);
 		});
 
-		// after 1s of hover, show the tooltip
 		this.addEventListener('mouseleave', () => {
-			this.hover = false;
-			setTimeout(() => {
-				//this.tt.removeAttribute('visible');
-				//this.showTooltip();
-				this.hideTooltip();
-			}, window.tooltipCfg.hideDelay);
+			this.setTimeout(() => this.hideTooltip(), tooltipCfg.hideDelay);
 		});
-		//this.addEventListener('mouseenter', this.showTooltip);
-		//this.addEventListener('mouseleave', this.hideTooltip);
 	}
 
-	tt() {
-		let tt = this.getElementsByTagName('tooltip-popup')[0];
+	removeTimeout() {
+		if (!this.timeout) return;
+
+		clearTimeout(this.timeout);
+		this.timeout = null;
+	}
+
+	/**
+	 * @param { () => void } f
+	 * @param { number } delay
+	 */
+	setTimeout(f, delay) {
+		this.removeTimeout();
+		this.timeout = setTimeout(f, delay);
+	}
+
+	/**
+	 * @returns { HTMLElement | undefined }
+	 */
+	popup() {
+		let tt = /** @type { HTMLElement | undefined }*/(this.getElementsByTagName('tooltip-popup')[0]);
 		return tt;
 	}
 
-	showTooltip = () => {
-		let tt = this.tt();
-		if (!tt) return;
-		if (!this.hover) return;
-		tt.setAttribute('_visible', '');
+	wait(delay = 0) {
+		return new Promise(resolve => setTimeout(resolve, delay));
+	}
 
-		let to = this.offsetTop + this.offsetHeight + 5;
-		let lo = this.offsetLeft + this.offsetWidth / 2;
+	showTooltip() {
+		let popup = this.popup();
+		if (!popup) return;
 
-		tt.style.top = to + 'px';
-		tt.style.left = lo + 'px';
+		this.setPopupPosition(popup);
+		popup.setAttribute('pre_visible', '');
 
-		let rect = tt.getBoundingClientRect();
-
-		if (rect.left < 0) {
-			tt.style.left = (lo - rect.left + 5) + 'px';
-		}
-
-		if (rect.right > window.innerWidth) {
-			tt.style.left = (lo - (rect.right - window.innerWidth) - 5) + 'px';
-		}
+		// wait for the tooltip to become block
+		// https://stackoverflow.com/questions/21539276/how-to-correctly-wait-until-javascript-applies-inline-css
+		this.wait().then(() => popup?.setAttribute('visible', ''));
 	}
 
 	hideTooltip = () => {
-		let tt = this.tt();
+		let tt = this.popup();
 		if (!tt) return;
-		if (this.hover) return;
-		tt.removeAttribute('_visible');
+		tt.removeAttribute('visible');
+		tt.removeAttribute('pre_visible');
+	}
+
+	/**
+	 * 
+	 * @param { HTMLElement } popup 
+	 */
+	setPopupPosition(popup) {
+		let to = this.offsetTop + this.offsetHeight + 5;
+		let lo = this.offsetLeft + this.offsetWidth / 2;
+
+		popup.style.top = to + 'px';
+		popup.style.left = lo + 'px';
+
+		let rect = popup.getBoundingClientRect();
+
+		if (rect.right > window.innerWidth) {
+			popup.style.left = (lo - (rect.right - window.innerWidth) - 5) + 'px';
+		}
+
+		if (rect.left < 0) {
+			popup.style.left = (lo - rect.left + 5) + 'px';
+		}
 	}
 }
 
